a6860291ed842ead08fbd65c0f4d47a669fc3077
Hakev <86327866+Hakev@users.noreply.github.com>
2022-11-30 18:53:10 +0300

    www.patika.dev

---
+++ harmoniksayi.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class harmoniksayi {
+    public static void main(String[] args){
+        Scanner input =new Scanner(System.in);
+        System.out.print("Lütfen Bir Sayı Giriniz : ");
+        int n = input.nextInt();
+        double result = 0;
+        for (double i = 1; i<=n; i++){
+            result *= (1/i);
+        }
+        System.out.print(result);
+    }
+}
